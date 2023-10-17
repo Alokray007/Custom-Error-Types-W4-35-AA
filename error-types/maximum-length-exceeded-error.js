4826cc5a5231fba63976b4785223f544ff8d0241
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-18 01:44:41 +0530

    1st commit

---
+++ error-types/maximum-length-exceeded-error.js
@@ -0,0 +1,19 @@
+const ValidationError = require('./validation-error');
+
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength ) {
+    const message = excessLength ? `Maximum length exceeded by ${excessLength}` : "Maximum length exceeded";
+    super(message);
+
+    this.name = "MaximumLengthExceededError";
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = MaximumLengthExceededError;
+} catch {
+  module.exports = null;
+}
